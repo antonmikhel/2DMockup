4bdedb19f8c0a5c0fba3fa6f81eb65bdf330164b
anton.mikhel <antonmikhel@gmail.com>
2017-03-19 21:59:35 +0200

    Wip on applying shadows to mockup

--- MockupFactory/MockFactory.py
+++ MockupFactory/MockFactory.py
@@ -22,7 +22,7 @@ class MockupGen(object):
         self.__log.info("MF instance initializing..")
         self.__name = name
         self.__prod_file_names = ["BabyBodySuit.jpg", "IphoneCase.jpg", "LadiesTank.jpg",
-                                  "MensTShirt.jpg", "PillowSquare.jpg", "Socks.jpg",
+                                  "t-shirt.png", "PillowSquare.jpg", "Socks.jpg",
                                   "Sweatshirt.jpg", "ToteBag.jpg"]
         self.__prod_dir = os.path.join(r"D:\Dev\Resources", "Products")
         self.__prod_mask_dir = os.path.join(self.__prod_dir, "Masks")
@@ -49,7 +49,7 @@ class MockupGen(object):
         prod_filenames = [_[0] for _ in prod_filenames]
         self.__log.info("Loading %d products from %s" % (len(prod_filenames), self.__logos_dir))
         self.__product_arr = [None for _ in range(len(prod_filenames))]
-        self.__product_arr = io_functions.load_img_array(prod_filenames, load_mode=cv2.CV_LOAD_IMAGE_COLOR, threads=6)
+        self.__product_arr = io_functions.load_img_array(prod_filenames, load_mode=cv2.CV_LOAD_IMAGE_UNCHANGED, threads=6)
         self.__log.info("Products loaded")
 
     def product_mask_loader(self):
@@ -95,14 +95,58 @@ class MockupGen(object):
 
         self.__log.info("Done applying logo #%d to product %d" % (logo_idx, prod_idx))
 
+    def apply_logo_with_shadows(self, logo_idx, prod_idx):
+
+        self.__log.info("Applying logo #%d to product %d" % (logo_idx, prod_idx))
+        prod = self.__product_arr[prod_idx].astype(numpy.float32)
+        prod_mask = self.__product_mask_arr[prod_idx].astype(numpy.float)
+        shadows = cv2.imread(r"D:\Dev\Resources\Products\Shadows\t-shirt.png", cv2.CV_LOAD_IMAGE_UNCHANGED)
+        logo = self.__logo_arr[logo_idx].astype(numpy.float)
+
+        first_row = numpy.transpose(prod_mask[:, :, 3].nonzero())[0][0]
+        last_row = numpy.transpose(prod_mask[:, :, 3].nonzero())[-1][0]
+
+        new_height = last_row - first_row
+        height_aspect = int(new_height / logo.shape[0])
+        new_width = int(logo.shape[1] * height_aspect)
+
+        logo = cv2.resize(logo, prod.shape[:-1][::-1])
+
+        alpha = numpy.full(prod.shape[:-1], 255.0, dtype=numpy.float)
+        alpha = alpha[:, :, numpy.newaxis]
+        if not prod.shape[2] == 4:
+            prod = numpy.concatenate((prod, alpha), axis=-1)
+        # if not prod_mask.shape[2] == 4:
+        #     prod_mask = numpy.concatenate((prod_mask, alpha), axis=-1)
+        if not logo.shape[2] == 4:
+            logo = numpy.concatenate((logo, alpha), axis=-1)
+
+        blended = blend_modes.multiply(prod_mask, logo, 1.0)
+        # blended = blend_modes.multiply(prod, blended, 0.8)
+        blended = blend_modes.multiply(prod, blended, 1.0)
+        # added = blend_modes.addition(no_shirt, blended, 1.0)
+
+        if 1:
+            curr_dir = os.path.join(r"D:\Dev\Resources\Tests", "%d" % logo_idx)
+            if not os.path.exists(curr_dir):
+                os.mkdir(curr_dir)
+
+            cv2.imwrite(os.path.join(curr_dir, "%d_%d.jpg" % (logo_idx, prod_idx)), blended.astype(numpy.uint8))
+
+            # cv2.namedWindow("window", cv2.WINDOW_NORMAL)
+            # cv2.imshow('window', blended.astype(numpy.uint8))
+            # cv2.waitKey()
+
+        self.__log.info("Done applying logo #%d to product %d" % (logo_idx, prod_idx))
+
     def run_factory(self):
         self.__log.info("Applying %d logos to %d products (%d operations)" % (len(self.__logo_arr),
                                                                               len(self.__product_arr),
                                                                               len(self.__logo_arr) *
                                                                               len(self.__product_arr)
                                                                               ))
-        for logo_idx in range(len(self.__logo_arr)):
-            for prod_idx in range(len(self.__product_arr)):
-                self.apply_logo(logo_idx, prod_idx)
+        # for logo_idx in range(len(self.__logo_arr)):
+        #     for prod_idx in range(len(self.__product_arr)):
+        #         self.apply_logo(logo_idx, prod_idx)
 
         self.__log.info("Done applying all logos to all products!")
